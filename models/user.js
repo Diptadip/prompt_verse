9eff20c8e726a61fd50008d3c11999f8eaa5bae2
Diptadip <diptadip2105@gmail.com>
2023-05-16 12:28:26 +0530

    added models for user and prompt

---
+++ models/user.js
@@ -0,0 +1,20 @@
+import { Schema,model,models } from "mongoose";
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        unique: [true,'Email already exists!'],
+        required: [true,'Please provide an email!'],
+    },
+    username: {
+        type: String,
+        required: [true,'Please provide a username!'],
+        match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"],
+    },
+    Image: {
+        type: String,
+    },
+})
+
+const User=models.User || model('User',UserSchema);
+export default User;
