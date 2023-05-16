b3a4705bcf338705fa64c9c407bfbcd2ce29ff89
Diptadip <diptadip2105@gmail.com>
2023-05-16 12:32:36 +0530

    new prompt api created

---
+++ app/api/prompt/new/route.js
@@ -0,0 +1,24 @@
+import {connectToDB} from '@utils/database'
+import Prompt from '@models/prompt'
+export const POST = async (req) => {
+    const {userId,prompt,tag}=await req.json();
+
+    try{
+        await connectToDB();
+        const newPrompt = await Prompt.create({
+            creator: userId,
+            prompt,
+            tag
+        });
+
+        await newPrompt.save();
+        return new Response(JSON.stringify(newPrompt),{
+            status: 201
+        })
+    }catch(error){
+        console.log("Error creating prompt: ",error);
+        return new Response(JSON.stringify(error),{
+            status: 500
+        })
+    }
+}
